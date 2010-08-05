751011c6b1f902dba4ce53d71b69b7fb271bfe28
Federico Ramallo <framallo@gmail.com>
2010-08-04 21:44:34 -0500

    fixed migration

--- db/migrate/20100313132032_add_issues_nested_sets_columns.rb
+++ db/migrate/20100313132032_add_issues_nested_sets_columns.rb
@@ -1,11 +1,22 @@
 class AddIssuesNestedSetsColumns < ActiveRecord::Migration
   def self.up
-    add_column :issues, :parent_id, :integer, :default => nil
-    add_column :issues, :root_id, :integer, :default => nil
-    add_column :issues, :lft, :integer, :default => nil
-    add_column :issues, :rgt, :integer, :default => nil
-    
-    Issue.update_all("parent_id = NULL, root_id = id, lft = 1, rgt = 2")
+    # when the database already used redmine_subtasks plugin
+    if ['parent_id','lft','rgt'].all? {|c| Issue.column_names.include?(c) }
+      add_column :issues, :root_id, :integer, :default => nil
+      Issue.reset_column_information
+      
+      Issue.update_all("parent_id = NULL, root_id = id, lft = 1, rgt = 2", "parent_id IS NULL")
+      Issue.find(:all,:conditions=>"parent_id IS NOT NULL").each do |issue|
+         issue.update_attribute(:parent_issue_id, issue.parent_id)
+      end
+    else
+      add_column :issues, :parent_id, :integer, :default => nil
+      add_column :issues, :root_id, :integer, :default => nil
+      add_column :issues, :lft, :integer, :default => nil
+      add_column :issues, :rgt, :integer, :default => nil
+
+      Issue.update_all("parent_id = NULL, root_id = id, lft = 1, rgt = 2")
+    end
   end
 
   def self.down
